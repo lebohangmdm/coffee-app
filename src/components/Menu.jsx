961227ff794866c17e6bab0f61e6ee747786aefd
lebohang_mdm <lebohangmdm@gmail.com>
2024-03-16 17:10:09 +0200

    fix filtering and sorting bug

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,30 +1,30 @@
 import { useState } from "react";
 import { coffeeData } from "../../data";
 import Coffee from "./Coffee";
-import { useEffect } from "react";
 
 const categories = new Set(coffeeData.map((coffee) => coffee.category));
 const allCategories = ["all", ...categories];
 
 const Menu = () => {
-  const [menu, setMenu] = useState(coffeeData);
-  const [filter, setFilter] = useState("");
+  const [menuItems, setMenuItems] = useState(coffeeData);
+  const [categories, setCategories] = useState(allCategories);
+  const [sortBy, setSortBy] = useState("input");
 
-  useEffect(() => {
-    const filterMenu = () => {
-      let filteredMenu = coffeeData;
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(coffeeData);
+      return;
+    }
+    const newItems = coffeeData.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
-      if (filter) {
-        filteredMenu =
-          filter === "all"
-            ? filteredMenu
-            : filteredMenu.filter((item) => item.category === filter);
-      }
-
-      setMenu(filteredMenu);
-    };
-    filterMenu();
-  }, [filter]);
+  let sortedItems;
+  if (sortBy === "input") sortedItems = menuItems;
+  if (sortBy === "lowest")
+    sortedItems = menuItems.sort((a, b) => a.price - b.price);
+  if (sortBy === "highest")
+    sortedItems = menuItems.sort((a, b) => b.price - a.price);
 
   return (
     <main className="menu">
@@ -39,23 +39,26 @@ const Menu = () => {
           </p>
 
           <div className="btn-container">
-            {allCategories.map((item, i) => {
+            {categories.map((category, i) => {
               return (
                 <button
                   key={i}
-                  className={`button ${
-                    item === filter ? "bg-brownish text-white" : ""
-                  }`}
-                  onClick={() => setFilter(item)}
+                  className={`button `}
+                  onClick={() => filterItems(category)}
                 >
-                  {item}
+                  {category}
                 </button>
               );
             })}
           </div>
 
           <div className="select-container">
-            <select className="select" name="sortBy">
+            <select
+              className="select"
+              name="sortBy"
+              value={sortBy}
+              onChange={(e) => setSortBy(e.target.value)}
+            >
               <option value="">Sort by</option>
               <option value="lowest">Sort by lowest</option>
               <option value="highest">Sort by highest</option>
@@ -63,7 +66,7 @@ const Menu = () => {
           </div>
 
           <ul className="coffees">
-            {menu.map((coffee) => {
+            {sortedItems.map((coffee) => {
               return <Coffee key={coffee.name} coffee={coffee} />;
             })}
           </ul>
